4840b373ca00f6f24099f7734bd255096d498a74
galbert0421 <131072989+galbert0421@users.noreply.github.com>
2023-04-17 19:10:34 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("School\n");
+    printf("Hello,School\n");
+  }
+  return 0;
+{
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello,School\n");
   }
   return 0;
 }
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello,School\n");
+  }
+  return 0;
+} 
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello,School\n");
+  }
+  return 0;
+}
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello,School\n");
+  }
+  return 0;
